7936907fab5f1b7bc800c4c2cc2f4f53f631e17a
SteveTangO <stevetanginchengdu@gmail.com>
2020-03-27 22:46:15 +0800

    add browsing history and profile favorites

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,9 @@
 import 'package:cuhk_treasure_hunt/utilities/constants.dart';
-import 'package:cuhk_treasure_hunt/widgets/homescreen_explore.dart';
-import 'package:flutter/material.dart';
 import 'package:cuhk_treasure_hunt/utilities/size_config.dart';
-import 'package:cuhk_treasure_hunt/widgets/homescreen_explore.dart';
 import 'package:cuhk_treasure_hunt/widgets/homescreen_chat_list.dart';
-
-
+import 'package:cuhk_treasure_hunt/widgets/homescreen_explore.dart';
+import 'package:cuhk_treasure_hunt/widgets/profile_browsing_history.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -21,14 +19,23 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Widget> _children = [
     //the color containers are only for debug use only
     HomescreenExplore(),
-    Container(color: Colors.green,),
+//    Container(
+//      color: Colors.green,
+//    ),
+    null,
     HomeScreenChat(),
     //Container(color: Colors.green,),
-    Container(color: Colors.red,),
-  ];  //the list of children widgets that will be rendered based on the choice
+    ProfileBrowsingHistory(),
+  ]; //the list of children widgets that will be rendered based on the choice
 
   final List<Widget> _childrenAppbar = [
-
+    null,
+    null,
+    null,
+    AppBar(
+      title: Text("Browsing"),
+      elevation: 0,
+    ),
   ];
 
   void _onItemTapped(int index) //set the index to the current index
@@ -39,45 +46,70 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget build(BuildContext context) {
-
-    SizeConfig().init(context); //initialize the size config object so all sizes can be adjusted
-
+    SizeConfig().init(
+        context); //initialize the size config object so all sizes can be adjusted
     return Scaffold(
-      resizeToAvoidBottomPadding: false,  //to avoid the bottom pixel overflow
+      resizeToAvoidBottomPadding: false, //to avoid the bottom pixel overflow
       //TODO: iOS and Android have different widgets
 //      appBar: AppBar(
 //        backgroundColor: Colors.white,
 //        title: Text("Tolo Dylamic", style: kappbartextstyle),
 //      ),
+      appBar: _childrenAppbar[_selectedIndex],
       body: SafeArea(
-          child: _children[_selectedIndex],
+        child: _children[_selectedIndex],
       ),
 
       bottomNavigationBar: BottomNavigationBar(
         items: [
-
           new BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: _selectedIndex == 0? klogin_button_color:knonactive_button_color,),
-            title: Text('Explore'),),
-
+            icon: Icon(
+              Icons.home,
+              color: _selectedIndex == 0
+                  ? klogin_button_color
+                  : knonactive_button_color,
+            ),
+            title: Text('Explore'),
+          ),
           new BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle, color: _selectedIndex == 1? klogin_button_color:knonactive_button_color,),
+            icon: Icon(
+              Icons.add_circle,
+              color: _selectedIndex == 1
+                  ? klogin_button_color
+                  : knonactive_button_color,
+            ),
             title: Text('Add'),
           ),
-
           new BottomNavigationBarItem(
-          icon: Icon(Icons.chat, color: _selectedIndex == 2? klogin_button_color:knonactive_button_color,),
-          title: Text('Chat'),),
-
+            icon: Icon(
+              Icons.chat,
+              color: _selectedIndex == 2
+                  ? klogin_button_color
+                  : knonactive_button_color,
+            ),
+            title: Text('Chat'),
+          ),
           new BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: _selectedIndex == 3? klogin_button_color:knonactive_button_color,),
-            title: Text('Me'),),
-
+            icon: Icon(
+              Icons.person,
+              color: _selectedIndex == 3
+                  ? klogin_button_color
+                  : knonactive_button_color,
+            ),
+            title: Text('Me'),
+          ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: klogin_button_color,
         onTap: _onItemTapped,
       ),
     );
+  }
+}
+
+class returnPostScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
