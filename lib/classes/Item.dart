a366e75a19767399dc88bb0f0b33f599b58094a9
1155124364 <1155124364@link.cuhk.edu.hk>
2020-04-20 23:34:35 +0800

    separate item_list widget into a stand-alone file

--- lib/classes/Item.dart
+++ lib/classes/Item.dart
@@ -4,7 +4,20 @@ import 'package:flutter/cupertino.dart';
 
 class Item{
 
-
+  String item_id;
+  String poster_id;
+  String name;
+  String price;
+  String quantity;
+  String create_time;
+  Item.fromJson(Map<String, dynamic> json) {
+    item_id = json["item_id"];
+    poster_id = json["poster_id"];
+    name = json["name"];
+    price = json["price"];
+    quantity = json["quantity"];
+    create_time = json["create_time"];
+  }
 
   // methods
   void updateBuyers(String type, String ID){
--- lib/widgets/homescreen_explore.dart
+++ lib/widgets/homescreen_explore.dart
@@ -2,6 +2,7 @@ import 'package:cuhk_treasure_hunt/screens/detail_screen.dart';
 import 'package:cuhk_treasure_hunt/screens/search_screen.dart';
 import 'package:cuhk_treasure_hunt/utilities/constants.dart';
 import 'package:cuhk_treasure_hunt/utilities/size_config.dart';
+import 'package:cuhk_treasure_hunt/widgets/item_list.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async';
@@ -140,97 +141,3 @@ class _ExploreBodyState extends State<ExploreBody> {
   }
 }
 
-class ItemGridView extends StatefulWidget {
-  @override
-  _ItemGridViewState createState() => _ItemGridViewState();
-}
-
-class _ItemGridViewState extends State<ItemGridView> {
-  @override
-
-  //determine if the item is favored or not
-  bool isFavorite = false;
-
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        child: Container(
-          height: SizeConfig.safeBlockVertical * 30,
-          width: SizeConfig.safeBlockHorizontal * 40,
-          child: Stack(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    color: Colors.amber,
-                    height: SizeConfig.safeBlockVertical * 20,
-                    width: SizeConfig.safeBlockHorizontal * 40,
-                  ),
-                  Container(
-                    child: Text("Title"),
-                    height: SizeConfig.safeBlockVertical * 2,
-                  ),
-                  Container(
-                    child: Text("Price"),
-                    height: SizeConfig.safeBlockVertical * 2,
-                  ),
-                  Container(
-                    child: Text("New Asia"),
-                    height: SizeConfig.safeBlockVertical * 2,
-                  ),
-                ],
-              ),
-              Positioned(
-                right: SizeConfig.safeBlockHorizontal * 3,
-                top: SizeConfig.safeBlockVertical * 22,
-                child: GestureDetector(
-                    onTap: () {
-                      isFavorite = !isFavorite;
-                      setState(() {});
-                    },
-                    child: isFavorite
-                        ? Icon(
-                            Icons.favorite,
-                            color: Colors.pink,
-                          )
-                        : Icon(
-                            Icons.favorite_border,
-                            color: Colors.pink,
-                          )),
-              ),
-            ],
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DetailScreen()),
-          );
-        });
-  }
-}
-
-// To return a widget that scrolls down the page to view the items
-class ItemListView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return _itemListView(context);
-  }
-
-  Widget _itemListView(BuildContext context) {
-    return ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ItemGridView(),
-              SizedBox(
-                width: SizeConfig.safeBlockHorizontal * 10,
-              ),
-              ItemGridView(),
-            ],
-          );
-        });
-  }
-}
