5a20641cf8fb882389c89f446811fed8c921be45
SteveTangO <Stevetanginchengdu@gmail.com>
2020-04-21 23:25:27 +0800

    solve merge problem and add chat contact

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -34,108 +34,115 @@ class LoginScreen extends StatelessWidget {
     String src = Database.hostname+"/data/images/image_picker_120C4BA2-E0F3-49EF-ADF8-4EDBD6A388C7-46791-0005A818D6B5A70F.jpg";
 
     return Scaffold(
-      resizeToAvoidBottomPadding: false,  //to avoid the bottom pixel overflow
+//      resizeToAvoidBottomPadding: false,  //to avoid the bottom pixel overflow
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: <Widget>[
-              //place the avatar of the user
-              SizedBox(
-                height: SizeConfig.safeBlockVertical*10,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                //place the avatar of the user
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical*10,
 
-              ),
-              Container(
-                height: SizeConfig.safeBlockVertical*30,
-                width: SizeConfig.safeBlockVertical*30,
-                child: Image.network(src,width: SizeConfig.safeBlockVertical*30,height: SizeConfig.safeBlockVertical*30),
-              ),
-              SizedBox(
-                height: SizeConfig.safeBlockVertical*5,
-              ),
-              //Input the username into the box
-              Container(
-                height: SizeConfig.safeBlockVertical*10,
-                width: SizeConfig.safeBlockHorizontal*80,
-                child: TextFormField(
-                  initialValue: username,
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.done,
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                      hintText: 'Username/Email'
+                ),
+                Container(
+                  height: SizeConfig.safeBlockVertical*30,
+                  width: SizeConfig.safeBlockVertical*30,
+                  child: Image.network(src,width: SizeConfig.safeBlockVertical*30,height: SizeConfig.safeBlockVertical*30),
+                ),
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical*5,
+                ),
+                //Input the username into the box
+                Container(
+                  height: SizeConfig.safeBlockVertical*10,
+                  width: SizeConfig.safeBlockHorizontal*80,
+                  child: TextFormField(
+                    initialValue: username,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.done,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                        hintText: 'Username/Email'
+                    ),
+                    onChanged: (value){
+                      username = value;
+                      print(username);
+                    },
                   ),
-                  onChanged: (value){
-                    username = value;
-                    print(username);
-                  },
                 ),
-              ),
-              //input the password into the box
-              SizedBox(
-                height: SizeConfig.safeBlockVertical*2,
-              ),
-              Container(
-                height: SizeConfig.safeBlockVertical*10,
-                width: SizeConfig.safeBlockHorizontal*80,
-                child: TextFormField(
-                  initialValue: password,
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.done,
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                      hintText: 'Password'
+                //input the password into the box
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical*2,
+                ),
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      height: SizeConfig.safeBlockVertical*10,
+                      width: SizeConfig.safeBlockHorizontal*80,
+                      child: TextFormField(
+//                        initialValue: password,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.done,
+                        maxLines: 1,
+//                        decoration: InputDecoration(
+//                            hintText: 'Password'
+//                        ),
+                        onChanged: (value){
+                          password = value;
+                          print(password);
+                        },
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      child: PasswordType(),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical*10,
+                ),
+                Container(
+                  color: Colors.orange,
+                  width: SizeConfig.safeBlockHorizontal*80,
+                  height: SizeConfig.safeBlockVertical*6,
+                  child: FlatButton(
+                    onPressed: ()  {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignupScreen()),
+                      );
+                    },
+                    child: Center(
+                        child: Text('Sign Up', style: klogin_button_text,)),
                   ),
-                  onChanged: (value){
-                    password = value;
-                    print(password);
-                  },
                 ),
-              ),
-              SizedBox(
-                height: SizeConfig.safeBlockVertical*10,
-              ),
-              Container(
-                color: Colors.orange,
-                width: SizeConfig.safeBlockHorizontal*80,
-                height: SizeConfig.safeBlockVertical*6,
-                child: FlatButton(
-                  onPressed: ()  {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignupScreen()),
-                    );
-                  },
-                  child: Center(
-                      child: Text('Sign Up', style: klogin_button_text,)),
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical*4,
                 ),
-              ),
-              SizedBox(
-                height: SizeConfig.safeBlockVertical*4,
-              ),
-              Container(
-                color: klogin_button_color,
-                width: SizeConfig.safeBlockHorizontal*80,
-                height: SizeConfig.safeBlockVertical*6,
-                child: FlatButton(
-                  onPressed: ()  async {
+                Container(
+                  color: klogin_button_color,
+                  width: SizeConfig.safeBlockHorizontal*80,
+                  height: SizeConfig.safeBlockVertical*6,
+                  child: FlatButton(
+                    onPressed: ()  async {
+//                      var user = await Database.get("/data/profile.php","");
+//                      print(json.decode(user.body));
+                      //the login process has been moved to loading screen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoadingScreen(
+                          username: username,password: password,),),
+                      );
 
-
-                    var user = await Database.get("/data/profile.php","");
-                    print(json.decode(user.body));
-
-                    //the login process has been moved to loading screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoadingScreen(
-                        username: username,password: password,),),
-                    );
-
-                  },
-                  child: Center(
-                      child: Text('Log In', style: klogin_button_text,)),
+                    },
+                    child: Center(
+                        child: Text('Log In', style: klogin_button_text,)),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
