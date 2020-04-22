85a636d87aee8d3b25f92aa7265d26cfa3b5b018
LI-Jialu <55438527+LI-Jialu@users.noreply.github.com>
2020-04-22 20:43:04 +0800

    alert_dialog and decideview

--- lib/classes/PostItem.dart
+++ lib/classes/PostItem.dart
@@ -3,6 +3,8 @@
 import 'dart:async';
 import 'dart:convert';
 //import 'dart:html' as html;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import "package:universal_html/prefer_sdk/html.dart" as html;
 import 'dart:typed_data';
@@ -15,27 +17,61 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
 class PostItem {
+  static var picture;
 
-  // methods
-  static void postItem(User user,String name, String posterID, double price, int quantity, List<String> tags){
-
-
-
+  // alert pickup image from web or gallery
+  static Future<File> showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Make a choice"),
+            content: SingleChildScrollView(
+                child: ListBody(
+              children: <Widget>[
+                GestureDetector(
+                  child: Text("Gallery"),
+                  onTap: () {
+                    picture = pickImage();
+                    Navigator.of(context).pop(); // dismiss dialog
+                  },
+                ),
+                Padding(padding: EdgeInsets.all(8.0)),
+                GestureDetector(
+                  child: Text("Web upload"),
+                  onTap: () {
+                    //file = uploadImageWeb();
+                    Navigator.of(context).pop(); // dismiss dialog
+                  },
+                ),
+              ],
+            )),
+          );
+        });
   }
 
+  static Widget decideImageView() {
+    if (picture == null) {
+      return Text("You havn't selected any pictures.");
+    } else {
+      return Image.file(picture);
+    }
+  }
 
-  static void uploadImage(File imageFile)async{
+  // methods
+  static void postItem(User user, String name, String posterID, double price,
+      int quantity, List<String> tags) {}
 
+  static void uploadImage(File imageFile) async {
     // upload image to server give a File.
-
     print("start uploading");
-    print (imageFile == null);
+    print(imageFile == null);
     var basename = imageFile.path.split('/').last;
     print(basename);
-    await http.post(Database.hostname+"/data/uploadImage.php",body:{
-      "image":base64Encode(imageFile.readAsBytesSync()),
+    await http.post(Database.hostname + "/data/uploadImage.php", body: {
+      "image": base64Encode(imageFile.readAsBytesSync()),
       "name": basename
-    }).then((value){
+    }).then((value) {
       if (value.statusCode == 200) {
         print(value.body);
         print("image uploaded");
@@ -43,75 +79,65 @@ class PostItem {
     });
   }
 
-  static Future<File> pickImage()async {
-
+  static Future<File> pickImage() async {
     // pick an image from phone gallery.
     // DO NOT USE THIS FOR WEB. for web, use uploadImageWeb below
-
     try {
-        var file = await ImagePicker.pickImage(source: ImageSource.gallery,maxHeight: 480,maxWidth: 480);
-        return file;
-    }
-    catch(e){
+      var file = await ImagePicker.pickImage(
+          //     source: ImageSource.gallery, maxHeight: 480, maxWidth: 480);
+          source: ImageSource.gallery);
+      return file;
+    } catch (e) {
       print("fail to select image");
       return null;
     }
   }
 
-  static void uploadImageWeb(){
-
+  static void uploadImageWeb() {
     // just calling this is okay
 
     html.InputElement uploadInput = html.FileUploadInputElement();
     uploadInput.click();
     uploadInput.accept = 'image/*';
     try {
-      uploadInput.onChange.listen((e){
+      uploadInput.onChange.listen((e) {
         var files = uploadInput.files;
         if (files.length == 1) {
           var reader = html.FileReader();
           reader.readAsArrayBuffer(files[0]);
           reader.onLoadEnd.listen((event) {
-             print(files[0].name);
-             //print(reader.result);
-             //add current time to file name to avoid duplicate
-             DateFormat dateFormat = DateFormat("yyyy-MM-dd-HH:mm:ss");
-             List<String> s = files[0].name.split(".");
-             uploadWeb(s[0]+dateFormat.format(DateTime.now())+"."+s[1], reader.result);
+            print(files[0].name);
+            //print(reader.result);
+            //add current time to file name to avoid duplicate
+            DateFormat dateFormat = DateFormat("yyyy-MM-dd-HH:mm:ss");
+            List<String> s = files[0].name.split(".");
+            uploadWeb(s[0] + dateFormat.format(DateTime.now()) + "." + s[1],
+                reader.result);
           });
-        }
-        else {
+        } else {
           print("more than 1 image selected.");
         }
-
       });
     } on Exception catch (e) {
       print("Fail to upload image.");
     }
   }
 
-  static void uploadWeb(String name, Uint8List bytes)async{
-
+  static void uploadWeb(String name, Uint8List bytes) async {
     // called inside uploadImageWeb, do not call this directly
 
-    try
-    {
+    try {
       //print(name);
-      await http.post(Database.hostname+"/data/uploadImage.php",body:{
-        "image":base64Encode(bytes),
-        "name": name
-      }).then((value){
+      await http.post(Database.hostname + "/data/uploadImage.php",
+          body: {"image": base64Encode(bytes), "name": name}).then((value) {
         if (value.statusCode == 200) {
           print(value.body);
           print("image uploaded");
         }
       });
-    }
-    catch(e){
+    } catch (e) {
       print(bytes == null);
       print("Failure");
     }
   }
-
-
 }
