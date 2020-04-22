1dd2796e272c3abc201ad6e0c63bf92b71d6aa80
LI-Jialu <55438527+LI-Jialu@users.noreply.github.com>
2020-04-22 16:38:44 +0800

    post_screen update

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -17,188 +17,187 @@ class PostScreen extends StatelessWidget {
     // TODO: implement build post screen
     SizeConfig().init(context);
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      appBar: AppBar(),
-      body: Column(
-        children: <Widget>[
-          Container(
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(),
+        body: SafeArea(
+          child: ListView(
+            //separatorBuilder: (BuildContext context, int index) => const Divider(),
+            //padding: const EdgeInsets.all(8),
             padding: EdgeInsets.only(
                 left: SizeConfig.safeBlockHorizontal * 10,
-                right: SizeConfig.safeBlockHorizontal * 5),
-            height: SizeConfig.safeBlockVertical * 5,
-            alignment: Alignment.bottomLeft,
-            child: Text('Title', style: ksmall_black_textstyle),
-          ),
-          Container(
-            height: SizeConfig.safeBlockVertical * 5,
-            width: SizeConfig.safeBlockHorizontal * 80,
-            child: TextField(
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.done,
-              maxLines: 1,
-              decoration: InputDecoration(hintText: 'Title of your item'),
-              onChanged: (value) {
-                titleInput = value;
-                print(titleInput);
-              },
-            ),
-          ),
-          Row(
+                right: SizeConfig.safeBlockHorizontal * 10),
+            scrollDirection: Axis.vertical,
             children: <Widget>[
               Container(
-                padding: EdgeInsets.only(
-                    left: SizeConfig.safeBlockHorizontal * 10,
-                    right: SizeConfig.safeBlockHorizontal * 5),
                 height: SizeConfig.safeBlockVertical * 5,
                 alignment: Alignment.bottomLeft,
-                child: Text('Bought price', style: ksmall_black_textstyle),
+                child: Text('Title', style: ksmall_black_textstyle),
               ),
               Container(
                 height: SizeConfig.safeBlockVertical * 5,
-                width: SizeConfig.safeBlockHorizontal * 50,
+                width: SizeConfig.safeBlockHorizontal * 80,
                 child: TextField(
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.done,
                   maxLines: 1,
-                  decoration:
-                      InputDecoration(hintText: 'The price you bought it.'),
+                  decoration: InputDecoration(hintText: 'Title of your item'),
                   onChanged: (value) {
-                    boughtPriceInput = value;
-                    print(boughtPriceInput);
+                    titleInput = value;
+                    print(titleInput);
                   },
                 ),
               ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
+              Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    height: SizeConfig.safeBlockVertical * 5,
+                    alignment: Alignment.centerLeft,
+                    child: Text('Bought price', style: ksmall_black_textstyle),
+                  ),
+                  Container(
+                    height: SizeConfig.safeBlockVertical * 5,
+                    width: SizeConfig.safeBlockHorizontal * 50,
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.done,
+                      maxLines: 1,
+                      decoration:
+                          InputDecoration(hintText: 'The price you bought it.'),
+                      onChanged: (value) {
+                        boughtPriceInput = value;
+                        print(boughtPriceInput);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    height: SizeConfig.safeBlockVertical * 5,
+                    alignment: Alignment.centerLeft,
+                    child: Text('Selling price', style: ksmall_black_textstyle),
+                  ),
+                  Container(
+                    height: SizeConfig.safeBlockVertical * 5,
+                    width: SizeConfig.safeBlockHorizontal * 50,
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.done,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                          hintText: 'The price you want to sell at'),
+                      onChanged: (value) {
+                        sellingPriceInput = value;
+                        print(sellingPriceInput);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
               Container(
-                padding: EdgeInsets.only(
-                    left: SizeConfig.safeBlockHorizontal * 10,
-                    right: SizeConfig.safeBlockHorizontal * 5),
+                height: SizeConfig.safeBlockVertical * 5,
+                alignment: Alignment.centerLeft,
+                child: Text('Description', style: ksmall_black_textstyle),
+              ),
+              Container(
+                child: Container(
+                  height: SizeConfig.safeBlockVertical * 20,
+                  width: SizeConfig.safeBlockHorizontal * 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 0.5),
+                  ),
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.done,
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                        hintText: 'Detailed information',
+                        border: InputBorder.none),
+                    onChanged: (value) {
+                      descriptionInput = value;
+                      print(descriptionInput);
+                    },
+                  ),
+                ),
+              ),
+              Divider(),
+              Container(
                 height: SizeConfig.safeBlockVertical * 5,
                 alignment: Alignment.bottomLeft,
-                child: Text('Selling price', style: ksmall_black_textstyle),
+                child: Text('Location', style: ksmall_black_textstyle),
               ),
               Container(
                 height: SizeConfig.safeBlockVertical * 5,
-                width: SizeConfig.safeBlockHorizontal * 50,
+                width: SizeConfig.safeBlockHorizontal * 80,
                 child: TextField(
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.done,
                   maxLines: 1,
-                  decoration: InputDecoration(
-                      hintText: 'The price you want to sell at'),
+                  decoration: InputDecoration(hintText: 'Delivering location'),
                   onChanged: (value) {
-                    sellingPriceInput = value;
-                    print(sellingPriceInput);
+                    locationInput = value;
+                    print(locationInput);
                   },
                 ),
               ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.only(
-                left: SizeConfig.safeBlockHorizontal * 10,
-                right: SizeConfig.safeBlockHorizontal * 5),
-            height: SizeConfig.safeBlockVertical * 5,
-            alignment: Alignment.bottomLeft,
-            child: Text('Description', style: ksmall_black_textstyle),
-          ),
-          Container(
-            padding: EdgeInsets.only(
-              left: SizeConfig.safeBlockHorizontal * 5,
-              right: SizeConfig.safeBlockHorizontal * 5,
-            ),
-            child: Container(
-              height: SizeConfig.safeBlockVertical * 20,
-              width: SizeConfig.safeBlockHorizontal * 80,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 0.5),
+              Divider(),
+              Container(
+                height: SizeConfig.safeBlockVertical * 5,
+                alignment: Alignment.centerLeft,
+                child: Text('Choose tags', style: ksmall_black_textstyle),
               ),
-              padding: EdgeInsets.only(
-                  left: SizeConfig.safeBlockHorizontal * 5,
-                  right: SizeConfig.safeBlockHorizontal * 5),
-              child: TextField(
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-                maxLines: 10,
-                decoration: InputDecoration(
-                    hintText: 'Detailed information', border: InputBorder.none),
-                onChanged: (value) {
-                  descriptionInput = value;
-                  print(descriptionInput);
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SizedBox(
+                    height: SizeConfig.safeBlockVertical * 5,
+                    width: SizeConfig.safeBlockHorizontal * 38,
+                    child: OutlineButton(
+                        //color: Colors.white,
+                        textColor: Colors.black,
+                        disabledTextColor: Colors.grey,
+                        padding: EdgeInsets.all(8.0),
+                        borderSide: BorderSide(color: Colors.grey, width: 0.5),
+                        onPressed: () {},
+                        child: Text("Brand new")),
+                  ),
+                  SizedBox(
+                    height: SizeConfig.safeBlockVertical * 5,
+                    width: SizeConfig.safeBlockHorizontal * 38,
+                    child: OutlineButton(
+                        color: Colors.white,
+                        textColor: Colors.black,
+                        disabledTextColor: Colors.grey,
+                        padding: EdgeInsets.all(8.0),
+                        borderSide: BorderSide(color: Colors.grey, width: 0.5),
+                        onPressed: () {},
+                        child: Text("free")),
+                  ),
+                ],
+              ),
+              Divider(),
+              FlatButton(
+                color: Colors.amber,
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                splashColor: Colors.blueAccent,
+                onPressed: () {
+                  /*...*/
                 },
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(
-                left: SizeConfig.safeBlockHorizontal * 10,
-                right: SizeConfig.safeBlockHorizontal * 5),
-            height: SizeConfig.safeBlockVertical * 5,
-            alignment: Alignment.bottomLeft,
-            child: Text('Location', style: ksmall_black_textstyle),
-          ),
-          Container(
-            height: SizeConfig.safeBlockVertical * 5,
-            width: SizeConfig.safeBlockHorizontal * 80,
-            child: TextField(
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.done,
-              maxLines: 1,
-              decoration: InputDecoration(hintText: 'Delivering location'),
-              onChanged: (value) {
-                locationInput = value;
-                print(locationInput);
-              },
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(
-                left: SizeConfig.safeBlockHorizontal * 10,
-                right: SizeConfig.safeBlockHorizontal * 5),
-            height: SizeConfig.safeBlockVertical * 5,
-            alignment: Alignment.bottomLeft,
-            child: Text('Choose tags', style: ksmall_black_textstyle),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 0.5),
+                child: Text(
+                  "Post",
+                  style: TextStyle(fontSize: 20.0),
                 ),
-                height: SizeConfig.safeBlockVertical * 5,
-                width: SizeConfig.safeBlockHorizontal * 30,
-                child: Center(
-                  child: Text("Brand new"),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 0.5),
-                ),
-                height: SizeConfig.safeBlockVertical * 5,
-                width: SizeConfig.safeBlockHorizontal * 30,
-                child: Center(
-                  child: Text("Brand new"),
-                ),
-              ),
+              )
             ],
           ),
-          Container(
-            color: klogin_button_color,
-            width: SizeConfig.safeBlockHorizontal * 80,
-            height: SizeConfig.safeBlockVertical * 6,
-            child: Center(
-                child: Text(
-              'Post',
-              style: klogin_button_text,
-            )),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
