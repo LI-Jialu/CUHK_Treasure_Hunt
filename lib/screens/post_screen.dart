bb938cb82477801aa3c48ed69764229fe1b08eec
httse1 <1155126684@link.cuhk.edu.hk>
2020-04-24 00:28:36 +0800

    fixed transaction history

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -211,7 +211,7 @@ class PostScreenState extends State<PostScreen> {
                       label: Text(Item.tags[index]),
                       selected: selected,
                       onSelected: (bool value) {
-                        if (!value) {
+                        if (!selected) {
                               setState(() {
                                 tags.add(index);
                               });
